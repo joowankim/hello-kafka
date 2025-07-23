8cde6c30e8916185851914e6c555af9dafac5403
Joowan Kim <32446834+joowankim@users.noreply.github.com>
2025-07-23 09:28:10 +0900

    d: 사용하지 않는 코드 제거

--- kafka/record.py
+++ kafka/record.py
@@ -1,10 +1,4 @@
-import base64
-import json
-from typing import Self
-
 from pydantic import BaseModel, Field
-
-from kafka import message
 
 
 class RecordMetadata(BaseModel):
@@ -21,18 +15,6 @@ class ProducerRecord(BaseModel):
     partition: int | None = None
     timestamp: int | None = None
     headers: dict[str, bytes] = Field(default_factory=dict)
-
-    @classmethod
-    def from_message(cls, msg: message.Message) -> Self:
-        if msg.headers.api_key != message.MessageType.PRODUCE:
-            raise ValueError("Message is not of type PRODUCE")
-        params = json.loads(msg.payload.decode("utf-8"))
-        if (value := params.get("value")) is None:
-            raise ValueError("ProducerRecord must have a 'value' field")
-        params["value"] = base64.b64decode(value)
-        if (key := params.get("key")) is not None:
-            params["key"] = base64.b64decode(key)
-        return cls.model_validate(params)
 
 
 class ConsumerRecord(BaseModel):
