929d6d6c281f1ce66a7bd91e3899108cac251784
Joowan Kim <32446834+joowankim@users.noreply.github.com>
2025-07-29 10:49:20 +0900

    r: 의존성 주입이 명시적으로 선언되도록 수정

--- kafka/broker/server.py
+++ kafka/broker/server.py
@@ -1,12 +1,46 @@
-import asyncio
+from pathlib import Path
+from kafka import constants, message
+from kafka.broker.router import Router
+from kafka.broker import handler
 
-from kafka.broker import parser
-from kafka.broker.handler import router
+import asyncio
+import functools
+
+from kafka.broker import parser, storage
 
 
 async def handle_client(
     reader: asyncio.StreamReader, writer: asyncio.StreamWriter
 ) -> None:
+    log_storage = storage.FSLogStorage.load_from_root(
+        Path("tmp"), constants.LOG_FILE_SIZE_LIMIT
+    )
+    committed_offset_storage = storage.FSCommittedOffsetStorage.load_from_root(
+        Path("tmp")
+    )
+    router = Router()
+    router.register(
+        message.MessageType.CREATE_TOPICS,
+        functools.partial(handler.create_topics, log_storage=log_storage),
+    )
+    router.register(
+        message.MessageType.PRODUCE,
+        functools.partial(handler.produce, log_storage=log_storage),
+    )
+    router.register(
+        message.MessageType.FETCH,
+        functools.partial(handler.fetch, log_storage=log_storage),
+    )
+    router.register(
+        message.MessageType.OFFSET_COMMIT,
+        functools.partial(
+            handler.offset_commit, committed_offset_storage=committed_offset_storage
+        ),
+    )
+    router.register(
+        message.MessageType.LIST_TOPICS,
+        functools.partial(handler.list_topics, log_storage=log_storage),
+    )
     message_parser = parser.MessageParser(reader)
     try:
         async for msg in message_parser:
