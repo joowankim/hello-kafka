9967f3f40608f12eca66cfbe6891b1d02b3e59cb
Joowan Kim <32446834+joowankim@users.noreply.github.com>
2025-07-29 09:50:01 +0900

    r: 핸들러 분리

--- kafka/broker/server.py
+++ kafka/broker/server.py
@@ -1,32 +1,16 @@
 import asyncio
-from pathlib import Path
 
-from kafka import constants, message
-from kafka.broker import storage, parser, handler
+from kafka.broker import parser
+from kafka.broker.handler import router
 
 
 async def handle_client(
     reader: asyncio.StreamReader, writer: asyncio.StreamWriter
 ) -> None:
-    log_storage = storage.FSLogStorage.load_from_root(
-        Path("tmp"), constants.LOG_FILE_SIZE_LIMIT
-    )
-    committed_offset_storage = storage.FSCommittedOffsetStorage.load_from_root(
-        Path("tmp")
-    )
     message_parser = parser.MessageParser(reader)
-    command_handler = handler.CommandHandler(log_storage, committed_offset_storage)
-    query_handler = handler.QueryHandler(log_storage)
-    handlers = {
-        message.MessageType.CREATE_TOPICS: command_handler,
-        message.MessageType.PRODUCE: command_handler,
-        message.MessageType.OFFSET_COMMIT: command_handler,
-        message.MessageType.FETCH: query_handler,
-        message.MessageType.LIST_TOPICS: query_handler,
-    }
     try:
         async for msg in message_parser:
-            resp = handlers[msg.headers.api_key].handle(msg)
+            resp = router.route(msg)
             writer.write(resp.serialized)
             await writer.drain()
     except asyncio.CancelledError:
